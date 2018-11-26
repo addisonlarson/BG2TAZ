afb1fa6c7fde3a468e12f3407a11fc23b4b2fadf
Addison Larson <39769450+addisonlarson@users.noreply.github.com>
2018-11-26 11:47:42 -0500

    happy edits, new input shapefiles, much to do

--- create_xwalk.R
+++ create_xwalk.R
@@ -1,14 +1,13 @@
-library(sf); library(dplyr); library(magrittr)
-setwd("D:/alarson/BG2TAZ")
+library(sf); library(dplyr); library(magrittr); library(here)
 bg <- read_sf("./bgs.shp") %>%
   select(GEOID10, ALAND10) %>%
   rename(bgID = GEOID10, bgAREA = ALAND10)
 bl <- read_sf("./blocks.shp") %>%
   select(GEOID10, ALAND10) %>%
   rename(blID = GEOID10, blAREA = ALAND10)
-taz <- read_sf("./dvrpc_taz.shp") %>%
-  select(TAZT, SHAPE_Area) %>%
-  rename(tazID = TAZT, tazAREA = SHAPE_Area)
+taz <- read_sf("./taz.shp") %>%
+  select(GEOID10, ALAND10) %>%
+  rename(tazID = GEOID10, tazAREA = ALAND10)
 
 bt <- st_intersection(bl, taz)
 bb <- st_intersection(bl, bg)
@@ -32,7 +31,7 @@ for(i in 1:length(splitByTAZ)){
 }
 
 # # as a test, take a subset of just one TAZ
-# bt %<>% filter(tazID == "22614") %>%
+# bt %<>% filter(tazID == "3401500024020") %>%
 #   mutate(weight = blAREA / sum(blAREA))
 # # block group contributions to this single TAZ
 # weight <- bt %>%
